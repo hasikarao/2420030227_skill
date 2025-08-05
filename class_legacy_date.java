27808cf58750b525f207947bed5031320ebc490a
hasikarao <2420030227@klh.edu.in>
2025-08-05 11:01:58 +0530

    Add files via upload

---
+++ class_legacy_date.java
@@ -0,0 +1,15 @@
+package Myproject;
+import java.util.*;
+
+public class class_legacy_date {
+public static void main(String args[]) {
+	Date d= new Date();
+	System.out.println("Current date is" +d);
+	int month =d.getMonth();
+	System.out.println("Month");
+	int year =d.getYear();
+	System.out.println(year);
+	System.out.println(d.getMinutes());
+	System.out.println(d.getHours());
+}
+}
