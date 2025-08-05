ab0dbdabda51eb4ffe23baaac9406cefb317438e
hasikarao <2420030227@klh.edu.in>
2025-08-05 10:29:16 +0530

    1. Type Conversion and Casting With Wrapper Class

---
+++ Wrapper.java
@@ -0,0 +1,14 @@
+package Myproject;
+
+public class Wrapper {
+	public static void main(String args[]) {
+		Integer a=564;
+		Boolean b=true;
+		Double d=9.8876;
+		int c=10;
+		System.out.println(a.toString().length());
+		System.out.println(d.toString().length());
+		
+	}
+
+}
